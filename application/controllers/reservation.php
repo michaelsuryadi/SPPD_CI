f9741dae37d6f3120f82df0d87c9c568e336b52a
Mike <Mike@Acer-PC>
2013-06-26 18:01:32 +0700

    Update CRUD

--- application/controllers/reservation.php
+++ application/controllers/reservation.php
@@ -2,4 +2,28 @@
 
 class Reservation extends CI_Controller{
     
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    function index(){
+        phpinfo();
+    }
+    
+    function flight_index(){
+        if ($this->session->userdata('username') != "") {
+            $data['title'] = 'Flight Reservation';
+            $this->load->model('employee');
+            $this->load->model('reservation_model');
+            $username = $this->session->userdata('username');
+            $data['result'] = $this->employee->get_detail_emp($username);
+            $data['airport'] = $this->reservation_model->get_list_airport();
+            echo ($data['airport']);
+            $data['mid_content'] = 'content/reservation/flight/pre_reservation';
+            $this->load->view('includes/home_template', $data);
+        }
+        else {
+            redirect("/login");
+        }
+    }
 }
--- application/models/reservation_model.php
+++ application/models/reservation_model.php
@@ -2,4 +2,41 @@
 
 class Reservation_model extends CI_Model {
     
+    function get_list_airport(){
+        $url = "http://login.pointer.co.id/api/airport/get/format/json";
+        $params = array(
+            'token'=>$this->token
+        );
+        
+        $json_result = json_decode($this->post($url,$params));
+        $return=$json_result;
+        if ($json_result != null && $json_result->status != null && $json_result->status->Code == "200") {
+            $json_airport = $json_result->pointer[0];
+            $return=$json_airport;
+        }
+        echo $return;
+    }
+    
+    function post($url, $params) {
+        try {
+            $post = json_encode($params);
+
+            $this->curl->create($url);
+            $this->curl->options(
+                array(
+                    CURLOPT_BUFFERSIZE => 128,
+                    CURLOPT_HTTPHEADER => array(
+                        'Content-Type: application/x-www-form-urlencoded',
+                        'Content-Length: '.strlen($post)
+                    )
+                )
+            );
+            $this->curl->post($post);
+            $result = $this->curl->execute();
+
+            return $result;
+        } catch (Exception $e) {
+            return null;
+        }
+    }
 }
